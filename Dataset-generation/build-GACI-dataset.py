a73422725d72079846125f29486ac2ca79281143
MaherDissem <maherdissem26@gmail.com>
2022-09-07 14:35:30 -0400

    added generation of 3 more features

--- Dataset-generation/build-GACI-dataset.py
+++ Dataset-generation/build-GACI-dataset.py
@@ -3,6 +3,8 @@ from requests.exceptions import HTTPError
 import pandas as pd
 import subprocess
 import os
+from math import log2
+from datetime import datetime
 
 USERNAME = 'MaherDissem'
 TOKEN = '' # Expires on Sat, Oct 22 2022. 
@@ -20,9 +22,11 @@ def get_features(owner,repo):
     features['la'] = []
     features['ld'] = []
     features['lt'] = []
+    features['is_fix'] = []
+    features['age'] = []
 
-    # res = subprocess.check_output(f"git clone https://github.com/{owner}/{repo}.git", shell=True)
-
+    os.chdir("/home/maher") # to avoid git dublious ownership error
+    res = subprocess.check_output(f"if cd {repo}; then git pull; else git clone https://github.com/{owner}/{repo}.git; fi", shell=True)
     try:
         page = 0
         count = 0; total = 0
@@ -35,7 +39,7 @@ def get_features(owner,repo):
             if l==0:
                 break
             for i in range(l):
-                
+                print(features)
                 # sha
                 sha = response[i]['sha']
                 features['sha_list'].append(sha)
@@ -46,7 +50,6 @@ def get_features(owner,repo):
 
                 # ci skipped                
                 if "CI SKIP" in commit_msg.upper() or "SKIP CI" in commit_msg.upper() or "CI-SKIP" in commit_msg.upper() or "SKIP-CI" in commit_msg.upper():
-                    # CI-skip feature = True
                     features['ci_skipped'].append(True)
                     count += 1
                 else:
@@ -89,16 +92,10 @@ def get_features(owner,repo):
                     features['mod_subsystems'].append(len(subsystems)) 
                 
                 # nbr of added lines
-                additions = 0
-                # for j in range(nbr_files):
-                #     additions += c_response['files'][j]['additions']
                 additions = c_response['stats']['additions']
                 features['la'].append(additions)
 
                 # nbr of deleted lines
-                deletions = 0
-                # for j in range(nbr_files):
-                #     deletions += c_response['files'][j]['deletions']
                 deletions = c_response['stats']['deletions']
                 features['ld'].append(deletions)
 
@@ -112,10 +109,34 @@ def get_features(owner,repo):
                 features['lt'].append(sum)
 
                 # entropy        
-                # calculate nbr of lines first
-                # features['entropy']
+                entropy = 0
+                total_changes = c_response['stats']['total']
+                for j in range(nbr_files):
+                    file_changes = c_response['files'][j]['changes']
+                    entropy -= file_changes/total_changes*log2(file_changes/total_changes)
+                features['entropy'] = entropy
 
+                # is_fix
+                if "FIX" in commit_msg.upper() or "PATCH" in commit_msg.upper() or "DEFECT" in commit_msg.upper() or "BUG" in commit_msg.upper():
+                    features['is_fix'].append(True)
+                else:
+                    features['is_fix'].append(False)
 
+                # history features
+                avg_time = 0
+                for j in range(nbr_files):
+                    file_path = c_response['files'][j]['filename']
+                    file_edit_date = c_response['commit']['committer']['date']
+
+                    file_mod_request = f"https://api.github.com/repos/{owner}/{repo}/commits?path={file_path}&until={file_edit_date}" #2022-06-01T23:59:59Z"
+                    dr = requests.get(file_mod_request, auth=(USERNAME, TOKEN))
+                    dr.raise_for_status()
+                    d_response = dr.json()
+
+                    prev_date = d_response[1]['commit']['committer']['date']
+                    delta_time = (datetime.strptime(file_edit_date,'%Y-%m-%dT%H:%M:%SZ') - datetime.strptime(prev_date,'%Y-%m-%dT%H:%M:%SZ')).total_seconds()/60
+                    avg_time += delta_time
+                features['age'].append(avg_time/nbr_files)
             page += 1
         print(f"{count/total*100:.2f}% of commits are CI-skipped")
         print(pd.DataFrame(features))
@@ -124,8 +145,6 @@ def get_features(owner,repo):
         print(f'HTTP error occurred: {http_err}')
     # except Exception as err:
     #     print(f'Other error occurred: {err}')
-
-#get_CM_list(owner="MaherDissem", repo="CI-SKIPPED-COMMITS-DETECTION")
 
 
 def uses_GA(owner,repo):
@@ -149,6 +168,6 @@ def uses_GA(owner,repo):
         print(f'Other error occurred: {err}')
 
 # uses_GA(owner="MaherDissem", repo="CI-SKIPPED-COMMITS-DETECTION")
-# uses_GA(owner="antongolub", repo="action-setup-bun")
-get_features(owner="MaherDissem", repo="CI-SKIPPED-COMMITS-DETECTION")
+get_features(owner="antongolub", repo="action-setup-bun")
+# get_features(owner="MaherDissem", repo="CI-SKIPPED-COMMITS-DETECTION")
 
